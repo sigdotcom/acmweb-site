eff8732bc5064d42db8a4ae4efd9820e6da41cdd
Malizma333 <malizma333@gmail.com>
2024-09-28 15:23:58 -0500

    integrated layouts, styles, and tailwind config from https://github.com/2406mmartin/acmweb-website-structure

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
+// @ts-check
 import { defineConfig } from 'astro/config';
 
-import react from '@astrojs/react';
+import tailwind from '@astrojs/tailwind';
+
+import solidJs from '@astrojs/solid-js';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+  integrations: [tailwind(), solidJs()],
+  devToolbar: {
+    enabled: false
+  }
 });
---
+++ tailwind.config.mjs
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			fontFamily: {
+			  roboto: ['Roboto', ...defaultTheme.fontFamily.sans],
+			  robotocondensed: ['Roboto Condensed', ...defaultTheme.fontFamily.sans]
+			}
+		  }
+	},
+	plugins: [],
+}
