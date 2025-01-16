7b36dd98c255401eaeb1431d167d843318228817
Malizma333 <malizma333@gmail.com>
2025-01-15 21:50:30 -0600

    static output config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,6 @@ import react from '@astrojs/react';
 export default defineConfig({
   integrations: [react()],
   site: 'https://sigdotcom.github.io',
-  base: 'acmweb-site'
+  base: 'acmweb-site',
+  output: 'static',
 });
